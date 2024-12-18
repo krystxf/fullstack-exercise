7620f232b1cd57ccdad854303b5ed0997cd57121
Krystof <krystof.kratky2003@gmail.com>
2024-12-18 09:21:09 +0100

    refactor: login page layout

---
+++ frontend/src/app/admin/(unauthorized)/login/layout.ts
@@ -0,0 +1,17 @@
+import { getTitle } from "@/utils/metadata.utils";
+import type { Metadata } from "next";
+import type { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: getTitle("Login"),
+};
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function AdminLoginPageLayout(props: Props) {
+  const { children } = props;
+
+  return children;
+}
